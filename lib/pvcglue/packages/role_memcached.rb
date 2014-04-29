83496e4d96d518b0b7f39b776f2a69c41887c1cf
Andrew Lyric <talyric@gmail.com>
2014-04-28 19:06:19 -0700

    memcached

---
+++ lib/pvcglue/packages/role_memcached.rb
@@ -0,0 +1,14 @@
+apt_package 'memcached'
+
+package 'caching' do
+  depends_on 'memcached'
+  file({
+           :template => Pvcglue.template_file_name('memcached.conf.erb'),
+           :destination => '/etc/memcached.conf',
+           :create_dirs => false,
+           :permissions => 0644,
+           :user => 'root',
+           :group => 'root'
+       }) { sudo('service memcached restart') }
+end
+
--- lib/pvcglue/packages/role_web.rb
+++ lib/pvcglue/packages/role_web.rb
@@ -1,5 +1,5 @@
 package 'web' do
-# rvm/ruby install based on http://ryanbigg.com/2010/12/ubuntu-ruby-rvm-rails-and-you/
+  # rvm/ruby install based on http://ryanbigg.com/2010/12/ubuntu-ruby-rvm-rails-and-you/
   depends_on 'build-essential'
   depends_on 'git'
   depends_on 'rvm'
@@ -13,21 +13,9 @@ package 'web' do
   depends_on 'app-env'
   depends_on 'web-site-config'
 
-  # depends_on 'imagemagick' # TODO:  app specific--will need to make system to include extra packages
-  # depends_on 'libpq-dev' # for pg gem
-  # depends_on 'nodejs'
-
-  # apply do
-  #   puts "*"*300
-  #   trigger 'nginx:restart' # files are copied first using 'depends_on' then we restart.
-  # end
-  #
-  # validate do
-  #   trigger('nginx:running')
-  #   #RestClient.get(node.test_url).code == 200 rescue false
-  # end
-
-
+  depends_on 'imagemagick' # TODO:  app specific--will need to make system to include extra packages
+  depends_on 'libpq-dev' # for pg gem
+  depends_on 'nodejs'
 end
 
 
